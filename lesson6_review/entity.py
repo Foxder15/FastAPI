c793dc078966dc0e0ace3affdcc527d125744f37
Thanh Duy <thanhduynt2604@gmail.com>
2026-04-25 00:35:22 +0700

    add post, put and delete user function

--- lesson6_review/entity.py
+++ lesson6_review/entity.py
@@ -9,15 +9,3 @@ class User:
 
     def __str__(self) -> str:
         return f"ID: {self.id}\nName: {self.name}\nPassword: {self.password}"
-    
-
-def mockup_data() -> None:
-    with open('mockup.txt', 'r') as f:
-        users = [f'{uuid.uuid1()}, {line.split(',')[0].strip()}, {line.split(',')[1].strip()}\n' for line in f]
-        with open('users.txt', 'w') as f:
-            f.writelines(users)
-
-if __name__ == "__main__":
-    # user: User = User('Baku', '@12345')
-    # print(user)
-    mockup_data()
--- lesson6_review/main.py
+++ lesson6_review/main.py
@@ -2,19 +2,31 @@
 from fastapi import FastAPI, Path, HTTPException
 from entity import User
 from pydantic import BaseModel
+import uuid
 
-# Create Model:
-
+# Create Base Model:
+class PostUser(BaseModel):
+    name: str
+    password: str
 
 # create FastAPI object.
 app = FastAPI()
 
 # Note that: 
 # Automactic docs - data validation - async/await - Fast to build is the key features of FastAPI
-    
+con = f'users.txt'
+
 def load_data():
-    with open('users.txt', 'r') as f:
+    with open(con, 'r') as f:
         return [User(line.split(',')[0], line.split(',')[1].strip(), line.split(',')[2].strip()) for line in f]
+    
+def add_data(data):
+    with open(con, 'a') as f:
+        f.write(data)
+
+def save_data(data):
+    with open(con, 'w') as f:
+        f.writelines(data)
 
 # routing my API
 @app.get("/users")
@@ -42,4 +54,34 @@ def get_user_by_id(user_id = Path(..., description='ID of the user'), example='1
 # It sets validaiton rule like numeric constraints (min/max value), regex patterns, min/max length, description - title of documentation. 
 # ... : the Ellipsis, means required.
 
+@app.post('/users')
+def create_user(user: PostUser):
+    store_user = User(uuid.uuid1(), user.name, user.password)
+    add_data(f"{store_user.id},{store_user.name},{store_user.password}\n")
+    return store_user
+
+@app.put('/users/{user_id}')
+def update_user(user_id: str, user: PostUser):
+    users = load_data()
+    for data_user in users:
+        if data_user.id == user_id:
+            update_user = User('NOT', user.name, user.password)
+            data_user.name = update_user.name
+            data_user.password = update_user.password
+            break
+
+    users_str = [f'{user.id},{user.name},{user.password}\n' for user in users]
+    save_data(users_str)
+    return data_user
+
+@app.delete('/users/{user_name}')
+def delete_user(user_name: str):
+    users = load_data()
+    for user in users:
+        if user.name == user_name:
+            users.remove(user)
+            break
+    users_str = [f'{user.id},{user.name},{user.password}\n' for user in users]
+    save_data(users_str)
+    return user
 
